5e0529a41b8c97adffb45318fd428fe2f384363c
ethaearn <chris@ethanet.co.nz>
2024-10-28 11:08:08 +1300

    Add files via upload

---
+++ veteransData-X.js
@@ -0,0 +1,25 @@
+const veteransDataX = [
+  {
+    "Surname": "Xuereb",
+    "Christian Name": "Juiliano",
+    "Unit": "3 Div A S C",
+    "Name Variation": "Sherab, Julian Sherab, Giuliano Sherab, Paul Julian Sherab, Julian Paul Xuereb, Juliano",
+    "Regtl. Number": "485957",
+    "Rank": "Dvr",
+    "Embarkation Dates": "Jan 43- Mar 43",
+    "Marital Status": "M",
+    "Enlistment": "Kawakawa",
+    "Occupation": "Driver mechanic",
+    "Address": "Kawakawa",
+    "Next of Kin": "Mrs C A Xuereb, 22 Station Rd, Kawakawa w",
+    "Last rank Held": "N K",
+    "Date of Birth": "Mon Aug 21 1911 193000 GMT1130 New Zealand Standard Time",
+    "Date of Death": "Sun Aug 04 1985 190000 GMT1200 New Zealand Standard Time",
+    "Age": "73",
+    "Husbands/Wives/Partners": "Alice Constance Constance Alice Wright",
+    "Place of birth": "Malta",
+    "Place of death": "NZ",
+    "AL": "Y",
+    "Notes": "Son of Emmanuel Josephus Andreas and Carmela Ludgarda Josepha Xuereb, nee Galea. Husband of Alice Constance (Constance Alice) Sherab, nee Wright, of Kawakawa. Wife s initials are C A in the NOK details. Name of Julian Paul Sherab is on the Death Index. Born in Malta. Place of death: New Zealand. Two profiles are on the Cenotaph database. Ancestrylibrary.com.au"
+  }
+];
